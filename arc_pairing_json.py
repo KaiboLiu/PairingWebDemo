7d275711a26a9ac920e13ad9e7d3e42d2b746e92
KaiboLiu <liukaibo87@gmail.com>
2017-12-28 21:23:05 -0800

    experiment, narrow the ref to () only as gold

--- arc_pairing_json.py
+++ arc_pairing_json.py
@@ -56,10 +56,10 @@ MAXLEN = 5650
 MINLEN = 0
 circular = True
 
-lbs = ['(', '[', '{', '<']
-rbs = [')', ']', '}', '>']
-#lbs = ['('] # pseudoknot-free only, even for grey part
-#rbs = [')'] # pseudoknot-free only, even for grey part 
+#lbs = ['(', '[', '{', '<']
+#rbs = [')', ']', '}', '>']
+lbs = ['('] # pseudoknot-free only, even for grey part
+rbs = [')'] # pseudoknot-free only, even for grey part 
 
 def LoadSave(RNAtype,seqNo):
     #RNAtype = "16s"
