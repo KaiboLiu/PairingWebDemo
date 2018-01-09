616d6922a577c404d7fc97db38b7f7ff9e73b077
KaiboLiu <liukaibo87@gmail.com>
2018-01-09 13:05:12 -0800

    update

---
+++ extract_seq_name.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+'''
+Developer: Kaibo(lrushx)
+Email: liukaib@oregonstate.edu
+Process Time: Jan 09, 2018
+ */
+'''
+
+'''
+# for WebDemo in RNA project,
+# original pairing structures are saved in different files by models and beams.
+# now I need to find sequence names for the display in web demo.
+# Sequence names are saved in ../RNA_visual_dengde/Mathews.clean.txt, which has 3857 sequence names and 11571 lines with seqs and refs.
+'''
+
+
+import sys
+import operator
+import pdb
+import os
+
+def LoadData():
+	RNAtype = "grp1"			# either 16s, 23s, or grp1
+	dataDir = "../RNA_visual_dengde/"
+	namePoolFile = "Mathews.clean.txt"
+	seqFile = "Mathewsdata."+RNAtype+".seq"
+
+	outDir  = "./seqName/"
+	nameFile = "seqNames."+RNAtype
+
+	if not os.path.exists(outDir):
+		os.makedirs(outDir)
+	
+	fileIn  = open(namePoolFile)
+	namePool = fileIn.readlines()	# the last char in namePool[i] is '\n'		
+	fileIn.close()
+
+	fileIn  = open(namePoolFile)
+	seqs = fileIn.readlines()		# the last char in seqs[i] is '\n'		
+	fileIn.close()
+	
+	f = open(outDir+outFile,"w")
+
+	for seq in seqs:
+		pos = namePool.index(seq)
+		f.write('seq: '+namePool[pos-1])
+	f.close()
+
+
+
+
+def RunMain():
+	print("start")
+	LoadData()
+
+if __name__ == "__main__":
+    RunMain()
