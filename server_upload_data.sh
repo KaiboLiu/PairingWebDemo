f67cf4a72d315b639c98cd13123a4eaa81516702
KaiboLiu <liukaibo87@gmail.com>
2018-01-26 02:43:49 -0800

    update

--- server_upload_data.sh
+++ server_upload_data.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 # scp files to OSU server for web demo
 
 scp -i ~/.ssh/OSU_server_rsa -r /Users/zhangyilin/0_pipi/LKB_OSU/Research/RNA/web_demo_canvas/demo_rearranged_results liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/public_html/demo_data/
