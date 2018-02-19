046c9a6b2bd35dbb3821f4236bc796a455734b79
Kaibo Liu <liukaibo87@gmail.com>
2018-02-19 14:02:04 -0800

    update

---
+++ server_end/linearfold/demo.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+# coding-utf8
+
+'''
+Developer: Kaibo(lrushx)
+Email: liukaib@oregonstate.edu
+Created Date: Jan 27, 2018
+'''
+
+import flask
+from werkzeug.utils import secure_filename 
+import os
+from time import time
+import re
+import json
+
+app = flask.Flask(__name__)
+
+fileDir = "/nfs/stak/users/liukaib/public_html/usrData/"
+#fileDir = os.path.join(os.getcwd(),"usrData")
+logFile = "/nfs/stak/users/liukaib/public_html/demo_data_run/usrLog.txt"
+logFile2 = "./usrLog.txt"
+pairingDir = "/nfs/stak/users/liukaib/public_html/demo_data_run/"
+
+demoURL = '/'
+@app.route(demoURL)
+def my_form():
+    #return flask.render_template('myform.html')
+    return flask.render_template('myform_2in1_input.html')
+
+#print flask.Flask.__doc__
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
+
+@app.route(demoURL, methods=['GET', 'POST'])
+def inputSeq():
+    if flask.request.method == 'POST':
+        input_flag = False
+        beamsize = flask.request.form['beamSize']
+        if not beamsize: beamsize = '100'
+        text = flask.request.form['seqInput']
+        filename = str(time()) + '_'
+        usrIP = flask.request.remote_addr
+        if text == '': 
+            if 'seqFile' not in flask.request.files:
+                return 'No input, nor selected file'
+            # upload part
+            file = flask.request.files['seqFile']
+            #filename += secure_filename(file.filename)
+            #filename = filename[:-4]
+            tmpPath = os.path.join(fileDir,'tmp')
+            file.save(tmpPath)
+            input_flag = True
+
+            with open(tmpPath) as f:
+                text = f.readlines()
+                if len(text) > 2 or len(text) < 1:
+                    os.system("rm -f {}".format(tmpPath))
+                    return 'wrong format in file'
+                seqName = text[0][1:-1] if text[0][0] == '>' else text[0][:-1]
+                seq     = text[-1][:-1]
+                if len(text) == 1: seqName = 'NoName' 
+            os.system("rm -f {}".format(tmpPath))
+        else: 
+            lineStop = text.find('\n')
+            if lineStop == -1:
+                return 'wrong input, supposed to be FASTA format'
+            # input part
+            input_flag = True
+            seqName = text[1:lineStop]
+            seq     = text[lineStop+1:]
+
+        if input_flag:
+            seq = seq.replace('\n','')
+            seq = seq.replace(' ','')
+            if ''.join(re.findall('[^AUCG]+',seq)):
+                return 'wrong input, only A/U/C/G is supposed in sequences'
+            print seq
+
+            seqName = secure_filename(seqName)
+            filename += seqName 
+            newPath = os.path.join(fileDir,filename)
+            f = open(newPath,'w')
+            f.write("{}\n{}\n{}".format(seqName, seq, beamsize))
+	    f.close()
+            resInfo, logInfo = request_ironcreek(filename)
+            addlog(logInfo, usrIP)
+            #return processed_text,newPath
+            with open(pairingDir+filename+'.pairing.res') as f:
+                pData = json.load(f)
+            return logInfo + '<br>name:&nbsp&nbsp' + seqName + '<br>seq:&nbsp&nbsp&nbsp&nbsp' + seq+'<br><br><br>'+resInfo.replace('\n','<br>')+'<br>'+pData['pairing'][6]+'<br><br>'+pData['pairing'][7]
+
+
+def addlog(logInfo, usrIP):
+    os.system("echo {} [IP: {}] >> {}".format(logInfo, usrIP, logFile))
+    os.system("cp {} {}".format(logFile, logFile2))
+
+
+def request_ironcreek(seqfile):
+    import socket               # import socket module
+    s = socket.socket()         # creat socket object
+    host = 'ironcreek.eecs.oregonstate.edu'
+    port = 11113                # set port
+    
+    s.connect((host, port))
+    s.send(seqfile)
+    response_s = s.recv(1024)
+    logInfo = s.recv(1024)
+    if '[' in response_s:
+        pos = response_s.index('[')
+        logInfo = response_s[pos:]
+        response_s = response_s[:pos]
+    s.close()
+    return response_s, logInfo
+
+if __name__ == '__main__':
+    #app.logger.debug('message processed')
+    app.logger.info('message processed')
+    #app.run(host='128.193.36.41', port=8001) #, debug=True) # flip.engr.oregonstate.edu
+    #app.run(host='73.67.241.185', port=8080) #, debug=True) # flop.engr.oregonstate.edu
+    #app.run(host='128.193.40.12', port=22) #, debug=True)  # web.engr.oregonstate.edu
+    app.run(host='128.193.38.37', port=5000, debug=True)  # linearfoldtest.eecs.oregonstate.edu
+    #app.run(host='0.0.0.0', port=8080)# , debug=True)
+    #app.run(host='0.0.0.0') #, debug=True)
+    #app.run(debug=True)
+    #app.run()
---
+++ server_end/linearfold/static/js/seqLoader.js
@@ -0,0 +1,32 @@
+$('#seqFile').bind('change', function () {
+//function loaded(){
+  var filename = $("#seqFile").val();
+  console.log(filename);
+  if (/^\s*$/.test(filename)) {
+    $(".file-upload").removeClass('active');
+    $("#noFile").text("No file chosen..."); 
+  }
+  else {
+    $(".file-upload").addClass('active');
+    //$("#noFile").text(filename);
+    $("#noFile").text(filename.replace("C:\\fakepath\\", "")); 
+
+        var form_data = new FormData($('#seqFile')[0]);
+        console.log(form_data);
+        $.ajax({
+            type: 'POST',
+            url: '/uploadajax',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: false,
+            success: function(data) {
+                console.log('Success!');
+            },
+        });
+  }
+//}
+});
+
+
